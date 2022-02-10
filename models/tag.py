080eccdfb2ff2f9630211b6df904a169a5da9973
DINI SHIBA S <67852680+DinishibaS@users.noreply.github.com>
2022-02-10 20:35:49 +0530

    Delete models directory

--- models/tag.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from base.models.base import BaseEntity
-
-
-class Tag(BaseEntity):
-    name = models.CharField(max_length=70)
-
-    def __str__(self):
-        return self.name
